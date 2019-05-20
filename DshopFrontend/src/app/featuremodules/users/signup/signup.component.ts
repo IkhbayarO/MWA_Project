55bec172d285cdf5d702663b943666135f200c10
sina15 <2038659554@qq.com>
2019-05-19 21:11:11 -0500

    changes

--- DshopFrontend/src/app/featuremodules/users/signup/signup.component.ts
+++ DshopFrontend/src/app/featuremodules/users/signup/signup.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/User';
+import { HttpClient } from '@angular/common/http';
 
-
+import { UserserviceService } from '../userservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,14 +12,28 @@ import { User } from '../model/User';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  //  user = new User('Asaad','Saad','asaad','asaad@mum.edu', '32323232')
+    
+  user:any ={}
+  temp:any ={}
+  newUser:object
+  serveUrl:string = 'http://localhost:3000/users/signup'
+  resp:object ={}
   
-  constructor() { }
-
+  
+  constructor(private service:UserserviceService) { }
+isAdded:boolean
   ngOnInit() {
   }
 
   onSubmit(form) {
-    console.log(form);
+    
+   this.temp = form.value; 
+   this.newUser = new User(this.temp.firstname,this.temp.lastname,this.temp.username,this.temp.email,this.temp.phone)
+   this.isAdded = this.service.createUser(this.serveUrl, this.newUser)
+
+   if(this.isAdded){
+     alert("Good Good!")
+   }
+
   }
 }
--- DshopFrontend/src/app/featuremodules/users/userservice.service.ts
+++ DshopFrontend/src/app/featuremodules/users/userservice.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviceService {
+  isAdded:boolean
+  isLogged:boolean
+  constructor(private http:HttpClient,private router:Router) { }
 
-  constructor() { }
+  createUser(serverUrl,user){
+     
+    this.http.post(serverUrl,{data:user}).subscribe(res=>{
+    
+
+      if(res.message=="success"){
+        alert("Added Successfully")
+        this.isAdded=true
+        this.router.navigate(['users'])
+      }
+      else{
+        this.isAdded=false
+      }
+
+    },err=>{
+      this.isAdded=false
+    })
+return this.isAdded;
+
+  }
+
+  loginUser(serverUrl,user){
+    alert('going to check')
+    this.http.post(serverUrl,{data:user}).subscribe(res=>{
+       
+      if(res.message=='success'){
+        console.log(res.data[0])
+        // localStorage.setItem('user',JSON.stringify(res.data))
+        this.isLogged=true
+        this.router.navigate([''])
+       }else{
+
+        this.isLogged=false
+       }
+    },err=>{
+
+      this.isLogged=false
+    })
+return this.loginUser
+  }
 }
