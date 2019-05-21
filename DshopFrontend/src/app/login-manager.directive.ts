590a1821f177cbfb19ce3789fd724b8e6ddd00ca
sina15 <2038659554@qq.com>
2019-05-21 14:52:19 -0500

    merged

--- DshopFrontend/src/app/login-manager.directive.ts
+++ DshopFrontend/src/app/login-manager.directive.ts
@@ -10,50 +10,13 @@ export class LoginManagerDirective implements OnInit {
 
 
   ngOnInit(){
-<<<<<<< HEAD
-      const token = localStorage.getItem('token')
-    console.log(token)
-    if(token!=null){
-
-    //  if(this.element.nativeElement.id=="loginNav"){
-    //    this.element.nativeElement.style.display='none'
-    //  }
-    //
-    //  if(this.element.nativeElement.id=="logoutNav"){
-    //   this.element.nativeElement.style.display='inline-block'
-    // }
-    //
-    // if(this.element.nativeElement.id=="navItemname"){
-    //   this.element.nativeElement.style.display='inline-block'
-    // }
-
-        this.element.nativeElement.style.display='none'
-
-=======
     console.log(localStorage.getItem('token'))
     if(localStorage.getItem('token')!=null){
 
       this.element.nativeElement.style.display='none'
->>>>>>> 38be8c3332d546424c3b81aa0e86a09d54429c1c
     }else{
       this.element.nativeElement.style.display='inline-block'
 
-<<<<<<< HEAD
-        this.element.nativeElement.style.display='inline-block'
-    //   if(this.element.nativeElement.id=="loginNav"){
-    //     this.element.nativeElement.style.display='inline-block'
-    //   }
-    //
-    //   if(this.element.nativeElement.id=="logoutNav"){
-    //    this.element.nativeElement.style.display='none'
-    //  }
-    //
-    //  if(this.element.nativeElement.id=="navItemname"){
-    //   this.element.nativeElement.style.display='none'
-    // }
-=======
-
->>>>>>> 38be8c3332d546424c3b81aa0e86a09d54429c1c
     }
   //   console.log(this.element.nativeElement.style)
   // this.element.nativeElement.style.display='none'
